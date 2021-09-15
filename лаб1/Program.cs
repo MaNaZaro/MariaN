be945471c074543dc8e562389e7757f9710b056c
NazarovaMaria <marianazarova0590@gmail.com>
2021-09-15 10:05:45 +0300

    no message

---
+++ лаб1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace ConsoleSettings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Информационной табло";
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Информация о системе:");
+
+            Console.WriteLine(Environment.MachineName);
+            Console.WriteLine(Environment.OSVersion);
+            Console.WriteLine(Environment.UserDomainName);
+            Console.WriteLine(Environment.UserName);
+            Console.WriteLine(Environment.Version);
+            Console.WriteLine(Environment.CurrentDirectory);
+            Console.WriteLine(Environment.CurrentManagedThreadId);
+            // Console.WriteLine(Environment.ProcessId);
+            Console.WriteLine(Environment.ProcessorCount);
+            Console.WriteLine(Environment.Is64BitProcess);
+            Console.WriteLine(Environment.Is64BitOperatingSystem);
+
+            Console.WriteLine();
+            Console.WriteLine(Console.ReadLine());
+        }
+    }
+}
